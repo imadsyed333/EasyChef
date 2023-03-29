b224c384d66a2777140c8d82acf13de529bccdf0
Imad <creeperone23@gmail.com>
2023-03-29 14:19:47 -0400

    Added buttons and links to other pages

---
+++ frontend/src/components/MenuBar.jsx
@@ -0,0 +1,24 @@
+
+const MenuBar = () => {
+    return (
+        <div>
+            <a href="/">
+                <button> Home </button>
+            </a>
+            <a href="/account/">
+                <button> Account </button>
+            </a>
+            <a href="/login/">
+                <button>Login</button>
+            </a>
+            <a href="/cart/">
+                <button>Shopping List</button>
+            </a>
+            <a href="/myrecipes/">
+                <button>My Recipes</button>
+            </a>
+        </div>
+    )
+}
+
+export default MenuBar
