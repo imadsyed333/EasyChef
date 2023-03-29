a8bae5ccc9759272e840a430a8d990a6543fe396
Imad <creeperone23@gmail.com>
2023-03-29 14:19:29 -0400

    Implemented routing for other pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AccountPage from "./pages/AccountPage";
+import ShoppingList from "./pages/ShoppingList";
+import LoginPage from "./pages/LoginPage";
+import MyRecipes from "./pages/MyRecipes";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route index element={<HomePage/>}/>
-          <Route path="/account/" element={<AccountPage/>}/>
+            <Route path="/myrecipes/" element={<MyRecipes/>}/>
+            <Route path="/login/" element={<LoginPage/>}/>
+            <Route path="/cart/" element={<ShoppingList/>}/>
+            <Route path="/account/" element={<AccountPage/>}/>
+            <Route index element={<HomePage/>}/>
         </Routes>
       </BrowserRouter>
   );
