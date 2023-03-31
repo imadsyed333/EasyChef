3888cb9dfdd779cba09d9d5217c18d7ca02323b0
Imad <creeperone23@gmail.com>
2023-03-31 15:57:36 -0400

    Changed route structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,19 @@ import AccountPage from "./pages/AccountPage";
 import ShoppingList from "./pages/ShoppingList";
 import LoginPage from "./pages/LoginPage";
 import MyRecipes from "./pages/MyRecipes";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-            <Route path="/myrecipes/" element={<MyRecipes/>}/>
-            <Route path="/login/" element={<LoginPage/>}/>
-            <Route path="/cart/" element={<ShoppingList/>}/>
-            <Route path="/account/" element={<AccountPage/>}/>
-            <Route index element={<HomePage/>}/>
+            <Route path="/">
+                <Route path="myrecipes" element={<MyRecipes/>}/>
+                <Route path="login" element={<LoginPage/>}/>
+                <Route path="cart" element={<ShoppingList/>}/>
+                <Route path="account" element={<AccountPage/>}/>
+                <Route index element={<HomePage/>}/>
+            </Route>
         </Routes>
       </BrowserRouter>
   );
