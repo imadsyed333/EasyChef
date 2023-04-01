82808c1624e1c607b2b95f6a323a085f62b1a4b5
Imad <creeperone23@gmail.com>
2023-04-01 18:51:04 -0400

    Moved recipe list fetching to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import MenuBar from "../components/MenuBar/MenuBar";
 import RecipeList from "../components/Recipe/RecipeList";
+import {useEffect, useState} from "react";
 
 const HomePage = () => {
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:8000/recipes/all/").then(response => response.json()).then(json => {
+            setRecipes(json.results)
+        })
+    }, [])
+
     return (
         <div>
             <h1>Welcome to EasyChef</h1>
-            <RecipeList/>
+            <RecipeList recipes={recipes}/>
         </div>
     )
 }
