838d705978bd80457f33920582f0a7ac68cefbf4
Imad <creeperone23@gmail.com>
2023-04-01 18:51:32 -0400

    Implemented basic recipe details page

--- frontend/src/pages/RecipeDetailsPage.jsx
+++ frontend/src/pages/RecipeDetailsPage.jsx
@@ -1,13 +1,81 @@
 import {useParams} from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import {useEffect, useState} from "react";
 
 
 const RecipeDetailsPage = () => {
 
     let {id} = useParams()
+    const [recipe, setRecipe] = useState({
+        diets: [],
+        cooking_time: 0,
+        cuisines: [],
+        prep_time: 0,
+        servings: 1,
+        steps: [],
+        comments: []
+    })
 
+    const [comment, setComment] = useState("")
+
+    useEffect(() => {
+        fetchRecipeDetails()
+    }, [])
+
+    const fetchRecipeDetails = () => {
+        fetch("http://localhost:8000/recipes/" + id + "/view/").then(response => response.json()).then(json => {
+            console.log(json)
+            setRecipe(json)
+        })
+    }
+
+    const sendComment = () => {
+        const data = {
+            poster: 1,
+            content: comment,
+            recipe: parseInt(id)
+        }
+        console.log("I am being called")
+        fetch("http://localhost:8000/recipes/comments/add/", {method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)}).then(response => response.json()).then(data => {console.log("Success:", data)
+        fetchRecipeDetails()}).catch(error => {
+            console.error("Error:", error)
+        })
+    }
 
     return (
-        <h1>this is where recipe {id} will be</h1>
+        <div>
+            <h1>{recipe.name}</h1>
+            <h2>Diets</h2>
+            <ul>
+
+            {recipe.diets.map(diet => (
+                <li key={diet.id}>{diet.name}</li>
+                )
+            )}
+            </ul>
+            <h2>Cuisines</h2>
+            <ul>
+                {recipe.cuisines.map(cuisine => (
+                    <li key={cuisine.id}>{cuisine.name}</li>
+                ))}
+            </ul>
+            <h2>Steps</h2>
+            <ol>
+                {recipe.steps.map(step => (
+                    <li key={step.id}>{step.content}</li>
+                ))}
+            </ol>
+            <h2>Comments Section</h2>
+            {recipe.comments.map(comment => (
+                <li>{comment.content}</li>
+            ))}
+            <textarea value={comment} onChange={(e) => setComment(e.target.value)}/>
+            <Button onClick={sendComment}>comment</Button>
+        </div>
     )
 }
 export default RecipeDetailsPage
