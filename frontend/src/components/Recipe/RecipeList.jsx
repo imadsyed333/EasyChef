5a942df65e2248b30c344f66a2cc87eb464be4a2
Imad <creeperone23@gmail.com>
2023-04-01 18:51:48 -0400

    Using props to render recipes

--- frontend/src/components/Recipe/RecipeList.jsx
+++ frontend/src/components/Recipe/RecipeList.jsx
@@ -1,19 +1,13 @@
 import {Button} from "react-bootstrap";
 import RecipeCard from "./RecipeCard";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
-const RecipeList = () => {
-    const [recipes, setRecipes] = useState([])
-    const getRecipes = () => {
-        fetch("http://localhost:8000/recipes/all/").then(response => response.json()).then(json => {
-            setRecipes(json.results)
-        })
-    }
+const RecipeList = (props) => {
+
     return (
         <div>
-            <Button onClick={getRecipes}> Get Recipes </Button>
             <h2> Here are your recipes! </h2>
-            {recipes.map(recipe => (
+            {props.recipes.map(recipe => (
                 <RecipeCard name={recipe.name} cooking_time={recipe.cooking_time} id={recipe.id}/>
             ))}
         </div>
