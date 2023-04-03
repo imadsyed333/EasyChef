74e15a06ac89a3fda43047777b32ca963898349a
Imad <creeperone23@gmail.com>
2023-04-02 23:48:13 -0400

    Implemented login functionality

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,67 @@
-import MenuBar from "../components/MenuBar/MenuBar";
+import AccountContext from "../contexts/AccountContext";
+import {useContext, useState} from "react";
+import Button from "react-bootstrap/Button";
 
 const LoginPage = () => {
+    const { setToken, setRefreshToken} = useContext(AccountContext)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState({})
+
+    const handleLogin = () => {
+        // https://stackoverflow.com/questions/46640024/how-do-i-post-form-data-with-fetch-api
+        const formData = new FormData()
+        formData.append("email", email)
+        formData.append("password", password)
+
+        fetch("http://localhost:8000/accounts/login/", {
+            method: "POST",
+            body: formData
+        }).then(response => {
+            if (response.status === 200) {
+                response.json().then(json => {
+                    setToken(json.access)
+                    setRefreshToken(json.refresh)
+                    setErrors({})
+                })
+            } else if (response.status === 400) {
+               response.json().then(json => setErrors(json))
+            }
+        }).catch(error => console.log(error))
+    }
+
+    const emailErrors = () => {
+        if (errors.email) {
+            return errors.email.map(error => (
+                <li>{error}</li>
+            ))
+        } else {
+            return <br/>
+        }
+    }
+
+    const passwordErrors = () => {
+        if (errors.password) {
+            return errors.password.map(error => (
+                <li>{error}</li>
+            ))
+        } else {
+            return <br/>
+        }
+    }
+
     return (
         <div>
-            Login here
+            Login here <br/>
+             <label>Username:
+                    <input type={"email"} name={"email"} value={email} onChange={event => setEmail(event.target.value)}/>
+                </label>
+            {emailErrors()}
+                <label>Password:
+                    <input type={"password"} name={"password"} value={password} onChange={event => setPassword(event.target.value)}/>
+                </label>
+            {passwordErrors()}
+                <Button onClick={handleLogin}>Login</Button>
         </div>
     )
 }
