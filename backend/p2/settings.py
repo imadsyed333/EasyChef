6d55e64ca2f0c2f5d569991935657fe4d7260749
Imad <creeperone23@gmail.com>
2023-04-02 23:49:51 -0400

    Removed csrfmiddleware

--- backend/p2/settings.py
+++ backend/p2/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
