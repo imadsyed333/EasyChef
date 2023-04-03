7db94ee1a61b031c740d54fa05cb132764d6c964
Imad <creeperone23@gmail.com>
2023-04-02 23:48:59 -0400

    Started functioning for fetching account info

--- frontend/src/components/MenuBar/MenuBar.jsx
+++ frontend/src/components/MenuBar/MenuBar.jsx
@@ -1,12 +1,23 @@
 import {Link, Outlet} from "react-router-dom";
 
 import {Container, Nav, Navbar} from "react-bootstrap";
+import {useContext, useEffect} from "react";
+import AccountContext from "../../contexts/AccountContext";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MenuBar = () => {
 
-    const getAccountInfo = () => {
-        // pass
-    }
+    const {token} = useContext(AccountContext)
+
+    useEffect(() => {
+        if (token.length > 0) {
+            fetch("http://localhost:8000/accounts/profile/view/", {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            }).then(response => response.json()).then(json => console.log("account:", json))
+        }
+    }, [token])
 
     return (
         <>
@@ -19,6 +30,9 @@ const MenuBar = () => {
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <Nav.Link as={Link} to="/myrecipes">My Recipes</Nav.Link>
                     <Nav.Link as={Link} to={"/cart"}>Shopping Cart</Nav.Link>
+                      <Nav className={"justify-content-end"}>
+                        <Nav.Link as={Link} to={"/login/"}>Login</Nav.Link>
+                      </Nav>
                   </Nav>
                 </Container>
             </Navbar>
