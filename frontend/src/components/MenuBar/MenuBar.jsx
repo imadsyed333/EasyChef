4742312004272a7d1130b610f7643a2305188c35
Imad <creeperone23@gmail.com>
2023-04-04 00:22:04 -0400

    Added more styling to menu bar, more logic for signing up/login

--- frontend/src/components/MenuBar/MenuBar.jsx
+++ frontend/src/components/MenuBar/MenuBar.jsx
@@ -1,13 +1,16 @@
 import {Link, Outlet} from "react-router-dom";
 
-import {Container, Nav, Navbar} from "react-bootstrap";
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import AccountContext from "../../contexts/AccountContext";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import {Navbar} from "react-bootstrap";
 
 const MenuBar = () => {
 
     const {token} = useContext(AccountContext)
+    const [user, setUser] = useState({})
 
     useEffect(() => {
         if (token.length > 0) {
@@ -15,25 +18,48 @@ const MenuBar = () => {
                 headers: {
                     "Authorization": "Bearer " + token
                 }
-            }).then(response => response.json()).then(json => console.log("account:", json))
+            }).then(response => response.json()).then(json => setUser(json))
         }
     }, [token])
+
+    const authLinks = () => {
+        if (token) {
+            return (
+
+                <Navbar.Collapse className={"justify-content-end"}>
+                    <Nav.Link as={Link} to={"/logout/"}>Log Out</Nav.Link>
+                    <NavBar.Text>
+                        Hello {user.first_name} !
+                    </NavBar.Text>
+                </Navbar.Collapse>
+
+            )
+        } else {
+            return (
+                <>
+                    <Navbar.Collapse className={"justify-content-end"}>
+                        <Nav.Link as={Link} to={"/login/"}> Login </Nav.Link>
+                        <br/>
+                        <Nav.Link as={Link} to={"/signup/"}> Sign Up </Nav.Link>
+                    </Navbar.Collapse>
+                </>
+            )
+        }
+    }
 
     return (
         <>
             <Navbar bg="light" variant="light">
                 <Container>
-                  <Navbar.Brand as={Link} to={"/"}>
-                      <img src="logo192.png" alt={""}/>
-                  </Navbar.Brand>
-                  <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/myrecipes">My Recipes</Nav.Link>
-                    <Nav.Link as={Link} to={"/cart"}>Shopping Cart</Nav.Link>
-                      <Nav className={"justify-content-end"}>
-                        <Nav.Link as={Link} to={"/login/"}>Login</Nav.Link>
-                      </Nav>
-                  </Nav>
+                    <Navbar.Brand as={Link} to={"/"}>
+                        <img src="logo192.png" alt={""}/>
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/myrecipes">My Recipes</Nav.Link>
+                        <Nav.Link as={Link} to={"/cart"}>Shopping Cart</Nav.Link>
+                    </Nav>
+                    {authLinks()}
                 </Container>
             </Navbar>
             <Outlet/>
