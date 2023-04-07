0582821c2a326c8dbcb647c94a6fa7635c9ca604
Agamjot Singh Saini <agamjot.saini@mail.utoronto.ca>
2023-04-07 01:40:02 -0400

    popular recipes (by overall rating or most fav'd)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,18 +3,33 @@ import RecipeList from "../components/Recipe/RecipeList";
 import {useEffect, useState} from "react";
 
 const HomePage = () => {
-    const [recipes, setRecipes] = useState([])
+    const [topratedrecipes, setTopRatedRecipes] = useState([])
+    const [mostfavouritedrecipes, setmostfavouritedrecipes] = useState([])
 
     useEffect(() => {
-        fetch("http://localhost:8000/recipes/all/").then(response => response.json()).then(json => {
-            setRecipes(json.results)
-        })
+        Promise.all([fetch("http://localhost:8000/recipes/overallratings/all/").then(response => response.json()).then(json => {
+            setTopRatedRecipes(json.results)
+        }),
+        fetch("http://localhost:8000/recipes/mostfavourited/all/").then(response => response.json()).then(json => {
+            setmostfavouritedrecipes(json.results)
+        })])
     }, [])
 
     return (
         <div>
             <h1>Welcome to EasyChef</h1>
-            <RecipeList recipes={recipes}/>
+
+            <h3>Popular Recipes:</h3>
+            <div>
+                <h4>Highest Overall Rating: </h4>
+                <RecipeList recipes={topratedrecipes}/>
+            </div>
+
+            <div>
+                <h4>Most Favourited: </h4>
+                <RecipeList recipes={mostfavouritedrecipes}/>
+            </div>
+
         </div>
     )
 }
