0a7a441174e654a1306666ab6fedc73c406a0c8f
Imad <creeperone23@gmail.com>
2023-04-08 01:45:11 -0400

    Implemented basic add recipe functionality

--- frontend/src/components/Forms/RecipeForm/RecipeForm.jsx
+++ frontend/src/components/Forms/RecipeForm/RecipeForm.jsx
@@ -1,8 +1,12 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import Button from "react-bootstrap/Button";
 import CuisineDietForm from "../CuisineDietForm/CuisineDietForm";
+import IngredientForm from "../IngredientForm/IngredientForm";
+import AccountContext from "../../../contexts/AccountContext";
 
 const RecipeForm = () => {
+    const {token} = useContext(AccountContext)
+
     const [name, setName] = useState("")
     const [prepTime, setPrepTime] = useState(0)
     const [cookingTime, setCookingTime] = useState(0)
@@ -12,28 +16,55 @@ const RecipeForm = () => {
     const [diets, setDiets] = useState([])
     const [ingredients, setIngredients] = useState([])
 
+    const handleAdd = () => {
+        const recipe = {
+            name: name,
+            prep_time: prepTime,
+            diets: diets,
+            cuisines: cuisines,
+            ingredients: ingredients,
+            cooking_time: cookingTime,
+            servings: servings
+        }
+        fetch("http://localhost:8000/recipes/create/", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(recipe)
+        }).then(response => response.json()).then(json => console.log(json))
+    }
+
     return (
         <div>
             <label>Name:
                 <input type={"text"} name={"name"} value={name} onChange={e => setName(e.target.value)}/>
             </label>
+            <br/>
             <label>Prep Time:
                 <input type={"number"} min={"0"} name={"prep_time"} value={prepTime}
                        onChange={e => setPrepTime(e.target.value)}/>
             </label>
+            <br/>
             <label>Cooking Time:
                 <input type={"number"} min={"0"} name={"cooking_time"} value={cookingTime}
                        onChange={e => setCookingTime(e.target.value)}/>
             </label>
+            <br/>
             <label>Servings:
                 <input type={"number"} min={"1"} name={"servings"} value={servings}
                        onChange={e => setServings(e.target.value)}/>
             </label>
+            <br/>
 
             <CuisineDietForm type={"cuisine"} items={cuisines} setItems={setCuisines}/>
+            <br/>
             <CuisineDietForm type={"diets"} items={diets} setItems={setDiets}/>
-
-            <Button>Add Recipe</Button>
+            <br/>
+            <IngredientForm ingredients={ingredients} setIngredients={setIngredients}/>
+            <br/>
+            <Button onClick={handleAdd}>Add Recipe</Button>
         </div>
     )
 
