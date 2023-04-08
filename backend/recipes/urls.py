b460720a07ac825dce0fda5deadbd007a210dcd5
Imad <creeperone23@gmail.com>
2023-04-08 01:46:48 -0400

    Added 'create' path for creating recipes with NewRecipeSerializer

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -7,7 +7,7 @@ from recipes.views import RecipeViewSet, DietViewSet, CommentViewSet, \
     SearchAPIView, StepViewSet, StepMediaViewSet, RecipeMediaViewSet, \
     FavouriteViewSet, MostFavouritedViewSet, OverallRatingViewSet, \
     LikeViewSet, MostLikedViewSet, MyFavouritesView, MyInteractionsView, \
-    MyRecipesView, CommentMediaViewSet
+    MyRecipesView, CommentMediaViewSet, RecipeView
 
 urlpatterns = [
     path('add/', RecipeViewSet.as_view({'post': 'create'})),
@@ -15,32 +15,34 @@ urlpatterns = [
     path('<int:pk>/delete/', RecipeViewSet.as_view({'get': 'destroy'})),
     path('all/', RecipeViewSet.as_view({'get': 'list'})),
 
+    path('create/', RecipeView.as_view()),
+
     path('diets/add/', DietViewSet.as_view({'post': 'create'})),
-    path('ingredients/add/', IngredientViewSet.as_view({'post':'create'})),
+    path('ingredients/add/', IngredientViewSet.as_view({'post': 'create'})),
     path('cuisines/add/', CuisineViewSet.as_view({'post': 'create'})),
     path('cuisines/all/', CuisineViewSet.as_view({'get': 'list'})),
 
-    path('media/add/', RecipeMediaViewSet.as_view({'post':'create'})),
+    path('media/add/', RecipeMediaViewSet.as_view({'post': 'create'})),
 
-    path('steps/add/', StepViewSet.as_view({'post':'create'})),
-    path('steps/media/add/', StepMediaViewSet.as_view({'post':'create'})),
+    path('steps/add/', StepViewSet.as_view({'post': 'create'})),
+    path('steps/media/add/', StepMediaViewSet.as_view({'post': 'create'})),
 
     path('comments/add/', CommentViewSet.as_view({'post': 'create'})),
     path('comments/media/add/', CommentMediaViewSet.as_view({'post': 'create'})),
     path('comments/all/', CommentViewSet.as_view({'get': 'list'})),
 
     path('ratings/add/', RatingViewSet.as_view({'post': 'create'})),
-    path('ratings/all/', OverallRatingViewSet.as_view({'get':'list'})),
+    path('ratings/all/', OverallRatingViewSet.as_view({'get': 'list'})),
 
     path('favourites/add/', FavouriteViewSet.as_view({'post': 'create'})),
-    path('favourites/all/', MostFavouritedViewSet.as_view({'get':'list'})),
+    path('favourites/all/', MostFavouritedViewSet.as_view({'get': 'list'})),
 
     path('favourites/', MyFavouritesView.as_view()),
     path('interactions/', MyInteractionsView.as_view()),
     path('recipes/', MyRecipesView.as_view()),
-    
+
     path('likes/add/', LikeViewSet.as_view({'post': 'create'})),
-    path('likes/all/', MostLikedViewSet.as_view({'get':'list'})),
+    path('likes/all/', MostLikedViewSet.as_view({'get': 'list'})),
 
     path('find/', SearchAPIView.as_view()),
 ]
