5aa69771199ce16003bf6b6891156c052e394533
Sachin <sachin.chhikara@mail.utoronto.ca>
2023-04-11 10:20:12 -0400

    set up the basic skelton of ingredient list

---
+++ frontend/src/components/ShoppingList/IngredientList.jsx
@@ -0,0 +1,37 @@
+import {useContext, useEffect, useState} from "react";
+import AccountContext from "../../contexts/AccountContext";
+
+
+const IngredientList = () => {
+    const {token} = useContext(AccountContext)
+    const [list, setList] = useState({})
+
+
+    useEffect(() => {
+        if (token) {
+            fetch("http://localhost:8000/accounts/ind_list/",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-type": "application/json",
+                        "Authorization": "Bearer " + token
+                    }
+
+                })
+                .then(data => data.json())
+                .then(d => {
+                    setList(Object.assign(list, d))
+                })
+
+        }
+    }, [token])
+
+
+    return (
+        <div>
+            {Object.keys(list)} : {list[Object.keys(list)[0]]}
+        </div>
+    )
+
+}
+export default IngredientList
