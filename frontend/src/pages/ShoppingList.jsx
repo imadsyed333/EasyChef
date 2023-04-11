de6eee7cce57d81cbfa9fec022b75d0d0d3e5d08
Sachin <sachin.chhikara@mail.utoronto.ca>
2023-04-11 08:31:07 -0400

    set up getting the shopping list data from the backend

--- frontend/src/pages/ShoppingList.jsx
+++ frontend/src/pages/ShoppingList.jsx
@@ -1,9 +1,57 @@
 import MenuBar from "../components/MenuBar/MenuBar";
+import RecipeCard from "../components/Recipe/RecipeCard";
+
+import {useContext, useEffect, useState} from "react";
+import AccountContext from "../contexts/AccountContext";
 
 const ShoppingList = () => {
+
+    const {token} = useContext(AccountContext)
+    // I need to get the name, media, servings
+    const [rname, setRname] = useState([]);
+    const [media, setMedia] = useState([]);
+    const [servings, setServings] = useState([]);
+
+
+
+
+
+    useEffect(() => {
+        if (token) {
+            fetch("http://localhost:8000/accounts/shopping_list/",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-type": "application/json",
+                        "Authorization": "Bearer " + token
+                    }
+
+                })
+                .then(data => data.json())
+                .then(a => {
+                    let n = []
+                    let m = []
+                    let s = []
+                    for (let i = 0; i < a.length; i++){
+                        let recipe = a[i]
+                        n.push(recipe.name)
+                        m.push(recipe.media[0]['media'])
+                        s.push(recipe.servings)
+                    }
+
+                    setRname(n)
+                    setMedia(m)
+
+                    setServings(s)
+                })
+
+        }
+    }, [token])
+
+
     return (
         <div>
-            This is the shopping list
+
         </div>
     )
 }
