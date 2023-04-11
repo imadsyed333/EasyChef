ef4a628f0ff74a337a88bfac3397f84a3c9d72ee
Imad <creeperone23@gmail.com>
2023-04-11 18:05:21 -0400

    Added mediaform not sure if it works though

---
+++ frontend/src/components/Forms/MediaForm/MediaForm.jsx
@@ -0,0 +1,18 @@
+import {useState} from "react";
+import Button from "react-bootstrap/Button";
+
+const MediaForm = ({media, setMedia}) => {
+
+
+    return (
+        <div>
+            <label>
+                Upload file:
+                {/*<input type={"file"} name={"file"} value={media} onChange={}/>*/}
+            </label>
+            <br/>
+        </div>
+    )
+}
+
+export default MediaForm
