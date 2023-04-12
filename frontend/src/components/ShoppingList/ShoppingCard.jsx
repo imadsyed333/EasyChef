5e088e0aff83485c1f659e85d8bbca574c9fc785
Imad <creeperone23@gmail.com>
2023-04-11 21:07:06 -0400

    Added ability to click to go to recipe page

--- frontend/src/components/ShoppingList/ShoppingCard.jsx
+++ frontend/src/components/ShoppingList/ShoppingCard.jsx
@@ -4,9 +4,12 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {CardActionArea, CardActions, Button} from '@mui/material';
 import {useContext, useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 const ShoppingCard = ({index, recipe, shoppingList, setShoppingList}) => {
     const [serving, setServing] = useState(recipe.servings)
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         let temp = [...shoppingList]
@@ -16,26 +19,23 @@ const ShoppingCard = ({index, recipe, shoppingList, setShoppingList}) => {
 
     return (
         <Card sx={{maxWidth: 345}}>
-            <CardActionArea>
+            <CardActionArea onClick={() => navigate('/recipe/' + recipe.id)}>
                 <CardMedia
                     component="img"
                     height="140"
                     alt={recipe.name}
                 />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {recipe.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Servings: <input type={"number"} min={1} onChange={e => setServing(e.target.value)}
-                                         value={serving}></input>
-                    </Typography>
-                </CardContent>
             </CardActionArea>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {recipe.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Servings: <input type={"number"} min={1} onChange={e => setServing(e.target.value)}
+                                     value={serving}></input>
+                </Typography>
+            </CardContent>
             <CardActions>
-                <Button>
-
-                </Button>
             </CardActions>
         </Card>
     )
