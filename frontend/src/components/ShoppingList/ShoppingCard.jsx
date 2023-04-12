d2bc5bc1aa7fd0a1d51f097b52a65d2523ee4d51
Imad <creeperone23@gmail.com>
2023-04-11 21:00:01 -0400

    Removed updating request

--- frontend/src/components/ShoppingList/ShoppingCard.jsx
+++ frontend/src/components/ShoppingList/ShoppingCard.jsx
@@ -1,63 +1,44 @@
-import {Link} from "react-router-dom";
-
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {CardActionArea, CardActions, Button} from '@mui/material';
-import {useContext} from "react";
-import AccountContext from "../../contexts/AccountContext";
+import {useContext, useEffect, useState} from "react";
 
-const RecipeCard = ({id, name, media, servings}) => {
-    const {token} = useContext(AccountContext)
-    const updateServings = () =>{
-        const postdata = {
-            recipe: id,
-            servings: servings,
-        }
+const ShoppingCard = ({index, recipe, shoppingList, setShoppingList}) => {
+    const [serving, setServing] = useState(recipe.servings)
 
-        if (token) {
-            fetch("http://localhost:8000/recipes/cart/update/",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json",
-                        "Authorization": "Bearer " + token
-                    },
-                    body: JSON.stringify(postdata)
-                })
-            console.log("Servings Updated")
-        }
-
-
-    }
-
+    useEffect(() => {
+        let temp = [...shoppingList]
+        temp[index].servings = serving
+        setShoppingList(temp)
+    }, [serving])
 
     return (
-        <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={media}
-          alt={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-              {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Servings: <input type={"number"} min={1} onChange={updateServings} defaultValue={servings}></input>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+        <Card sx={{maxWidth: 345}}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    alt={recipe.name}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {recipe.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Servings: <input type={"number"} min={1} onChange={e => setServing(e.target.value)}
+                                         value={serving}></input>
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
             <CardActions>
                 <Button>
 
                 </Button>
             </CardActions>
-    </Card>
+        </Card>
     )
 }
 
-export default RecipeCard
+export default ShoppingCard
